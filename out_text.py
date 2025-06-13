ce35b6cf78fe145547a91cea970d60dc8687e3aa
Karel Cervicek <cervicekkarel@gmail.com>
2025-06-14 00:34:16 +0200

    fix follow animation

--- out_text.py
+++ out_text.py
@@ -1,3 +1,4 @@
+
 import subprocess, glob
 import ffmpeg
 
@@ -8,13 +9,37 @@ start = round(duration - 5, 2)
 subprocess.run([
     "ffmpeg", "-y",
     "-i", video_file,
-    "-vf",
-    f"drawtext=fontfile=fonts/OpenSans-Bold.ttf:text='FOLLOW US':fontcolor=white:fontsize=80:x=(w-text_w)/2:y=100:"
-    f"enable='between(t,{start},{start+3})':alpha='if(lt(t,{start+1}),(t-{start})/1,1)',"
-    f"drawtext=fontfile=fonts/OpenSans-Regular.ttf:"
-    f"text='We’re documenting the entire creative journey. Join us.':fontcolor=white:fontsize=40:"
-    f"x=(w-text_w)/2:y=200:enable='between(t,{start+1},{duration})':alpha='if(lt(t,{start+2}),(t-{start+1})/1,1)'",
+    "-filter_complex",
+    f"""
+    drawtext=fontfile=fonts/OpenSans-Bold.ttf:
+    text='FOLLOW US':
+    fontcolor=white:fontsize=120:
+    x=(w-text_w)/2:
+    y='if(lt(t,{start+1}),(h*-0.2 + (h*0.4)*(t-{start})/1.2),h*0.2)':
+    enable='gte(t,{start})',
+
+    drawtext=fontfile=fonts/OpenSans-Regular.ttf:
+    text='Join our creative':
+    fontcolor=white:fontsize=48:
+    x='if(lt(t,{start+1.5}),-tw,(w-text_w)/2)':
+    y=h*0.45:
+    enable='between(t,{start+1.5},{start+4})',
+
+    drawtext=fontfile=fonts/OpenSans-Regular.ttf:
+    text='development journey':
+    fontcolor=white:fontsize=48:
+    x='if(lt(t,{start+1.8}),-tw,(w-text_w)/2)':
+    y=h*0.53:
+    enable='between(t,{start+1.8},{start+4.3})',
+
+    drawtext=fontfile=fonts/OpenSans-Regular.ttf:
+    text='from zero to launch.':
+    fontcolor=white:fontsize=48:
+    x='if(lt(t,{start+2.1}),-tw,(w-text_w)/2)':
+    y=h*0.61:
+    enable='between(t,{start+2.1},{start+4.6})'
+    """.replace('\n', ''),
     "-c:a", "copy",
     "-shortest",
-    "out_text.mp4"
+    "out_text_mobile.mp4"
 ])
