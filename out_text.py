a37ca2c02b99c445bb8408dcb6a7ef24847846eb
Karel Cervicek <cervicekkarel@gmail.com>
2025-06-14 15:48:32 +0200

    try better anim

--- out_text.py
+++ out_text.py
@@ -10,39 +10,39 @@ subprocess.run([
     "-i", video_file,
     "-filter_complex",
     f"""
-    drawtext=fontfile=fonts/OpenSans-Bold.ttf:
+    [0:v]drawtext=fontfile=fonts/OpenSans-Bold.ttf:
     text='FOLLOW US':
     fontcolor=white:fontsize=120:
     x=(w-text_w)/2:
-    y='if(lt(t,{start+1.2}), h*-0.5 + (h*0.7)*(1 - exp(-(t-{start})*3)), h*0.2)':
-    enable='gte(t,{start})',
+    y='h*-0.5 + (h*0.7)*(1 - exp(-((t-{start})*3)))':
+    enable='between(t,{start},{start+1.2})'[txt1];
 
-    drawtext=fontfile=fonts/OpenSans-Regular.ttf:
+    [txt1]drawtext=fontfile=fonts/OpenSans-Regular.ttf:
     text='Join our creative':
     fontcolor=white:fontsize=60:
     x='if(lt(t,{start+1.5}),-tw*1.2,(w-text_w)/2)':
     y=h*0.3:
-    enable='gte(t,{start+1.5})',
+    enable='gte(t,{start+1.5})'[txt2];
 
-    drawtext=fontfile=fonts/OpenSans-Regular.ttf:
+    [txt2]drawtext=fontfile=fonts/OpenSans-Regular.ttf:
     text='development journey':
     fontcolor=white:fontsize=60:
     x='if(lt(t,{start+1.8}),-tw*1.2,(w-text_w)/2)':
     y=h*0.35:
-    enable='gte(t,{start+1.8})',
+    enable='gte(t,{start+1.8})'[txt3];
 
-    drawtext=fontfile=fonts/OpenSans-Regular.ttf:
+    [txt3]drawtext=fontfile=fonts/OpenSans-Regular.ttf:
     text='from zero to launch.':
     fontcolor=white:fontsize=60:
     x='if(lt(t,{start+2.1}),-tw*1.2,(w-text_w)/2)':
     y=h*0.40:
-    enable='gte(t,{start+2.1})',
+    enable='gte(t,{start+2.1})'[txt4];
 
-    drawtext=fontfile=fonts/OpenSans-Bold.ttf:
+    [txt4]drawtext=fontfile=fonts/OpenSans-Bold.ttf:
     text='techart_society':
     fontcolor=#8ecae6:fontsize=50:
     x=(w-text_w)/2:
-    y=h*0.45:
+    y=h*0.47:
     enable='gte(t,{start+2.4})'
     """.replace('\n', ''),
     "-c:a", "copy",
